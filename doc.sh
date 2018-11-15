8f41669a782fd562686385c75708762d54924956
nearbydelta <cd4209@gmail.com>
2018-11-16 00:11:10 +0900

    Documentation of v2.0.1 at 11/16/18

--- doc.sh
+++ doc.sh
@@ -7,7 +7,7 @@ function run_sbt(){
   java -jar ~/.IdeaIC2018.2/config/plugins/Scala/launcher/sbt-launch.jar $@
 }
 
-run_sbt unidoc
+run_sbt doc
 git rm -r docs/api
 mv target/scala-2.12/api docs/api
 git add docs/api
